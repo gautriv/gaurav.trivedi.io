6dad56ef10bcfd577210f0db76538aa7e3279706
Gaurav Trivedi <trivedi.gaurav30@gmail.com>
2024-06-16 00:01:59 +0530

    Updated settings and pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className="relative z-1 max-w-[62rem]
         mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
             <h1 className="h1 mb-6">
-               Hello, I'm
+               And... I'm
                <br />
                <span className="inline-block relative">Gaurav Trivedi{" "}
                <img src={curve} 
