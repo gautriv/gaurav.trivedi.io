7f9e1f101a6ca8d14ebebefb8ec7a2c6dee554b5
Gaurav Trivedi <trivedi.gaurav30@gmail.com>
2024-06-16 17:54:27 +0530

    Updated images and settings

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,6 +4,7 @@ import Section from "./Section";
 import Arrow from '../assets/svg/Arrow';
 import { GradientLight } from "./design/Benefits";
 import ClipPath from '../assets/svg/ClipPath';
+import Button from './Button'; // Import the Button component
 
 const Services = () => {
   return (
@@ -23,19 +24,23 @@ const Services = () => {
               }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col h-full p-6 pointer-events-none">
+              <div className="relative z-2 flex flex-col h-full p-6">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
+                <div className="flex items-center mt-auto pointer-events-auto">
                   <img
                     src={item.iconUrl}
                     width={48}
                     height={48}
                     alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                  <Button
+                    href={item.exploreMoreUrl}
+                    className="ml-auto font-code text-xs font-bold uppercase tracking-wider"
+                    white
+                  >
                     Explore more
-                  </p>
+                  </Button>
                   <Arrow />
                 </div>
               </div>
